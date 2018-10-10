1749c298d32fbe067c998cfcef31807781f2529a
mosuswalks <mosuswalks@gmail.com>
2018-10-10 19:54:52 -0400

    two_sum.py added

---
+++ Interview_Questions/two_sum.py
@@ -0,0 +1,14 @@
+# Bonus:
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+
+def two_sum(array, target: int) -> []:
+    temp = {}
+    for x in range(len(array)):
+        if array[x] not in temp:
+            temp[array[x]] = x
+        if target - array[x] in temp.keys() and temp[target - array[x]] != x:
+            return [temp[target - array[x]], x]
+    return False
+
